74fba370f9ff1c935f694c5eaaa969bd392e6761
ytakaya <mt.tky@icloud.com>
2019-12-30 22:26:21 +0900

    envファイルにchannelを追加 #2

--- src/app/slack/postMessage.js
+++ src/app/slack/postMessage.js
@@ -2,12 +2,13 @@ require('dotenv').config();
 const { WebClient } = require('@slack/web-api');
 
 const token = process.env.OAUTH_ACCESS_TOKEN;
+const channel = process.env.CHANNEL;
 
 const web = new WebClient(token);
 
 (async () => {
   const result = await web.chat.postMessage({
     text: 'Hello world!',
-    channel: 'sampleapp',
+    channel: channel,
   });
 })();
