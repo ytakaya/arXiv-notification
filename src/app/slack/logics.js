2aec928f09a206ed97eda42075e1565925dbd0c7
ytakaya <mt.tky@icloud.com>
2020-01-03 10:29:39 +0900

    一定時間ごとにslackに通知を送る #6

--- src/app/slack/logics.js
+++ src/app/slack/logics.js
@@ -1,3 +1,9 @@
 const slack_notify = require('./slack_notify');
 
-slack_notify();
+function paper_notify_interval(hours) {
+  setInterval(() => {
+    slack_notify();
+  }, hours * 60 * 60 * 1000);
+};
+
+exports = module.exports = paper_notify_interval;
